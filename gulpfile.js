197be7bca29f66f09d707328a4957536905b3477
Geoffrey Vedernikoff <yefim323@gmail.com>
2016-03-25 14:32:22 -0700

    minify all the things

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var connect = require('gulp-connect');
 var less = require('gulp-less');
 var babel = require('gulp-babel');
+var uglify = require('gulp-uglify');
+var cssmin = require('gulp-cssmin');
 
 gulp.task('webserver', function() {
   connect.server({
@@ -12,13 +14,17 @@ gulp.task('webserver', function() {
 gulp.task('less', function() {
   gulp.src('styles/*.less')
     .pipe(less())
+    .pipe(cssmin())
     .pipe(gulp.dest('dist'))
     .pipe(connect.reload());
 });
 
 gulp.task('js', function() {
   gulp.src('scripts/*.js')
-    .pipe(babel())
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(uglify())
     .pipe(gulp.dest('dist'))
     .pipe(connect.reload());
 });
