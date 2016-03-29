449ebac22320e2733a7faf9f1d176fabafe71274
Geoffrey Vedernikoff <yefim323@gmail.com>
2016-03-29 02:21:39 -0700

    Forget it

--- scripts/app.js
+++ scripts/app.js
@@ -2,9 +2,6 @@ let $body = $('body');
 let $radial = $('.radial');
 let x, y, radial;
 
-// https://css-tricks.com/snippets/css/remove-gray-highlight-when-tapping-links-in-mobile-safari/
-document.addEventListener('touchstart', function(){}, true);
-
 $body.on('touchmove mousemove', (e) => {
   e.preventDefault();
 
