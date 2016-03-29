51b564d8e09045ad0c148340f2a922ff8fd625d2
Geoffrey Vedernikoff <yefim323@gmail.com>
2016-03-29 02:05:59 -0700

    Support mobile taps

--- scripts/app.js
+++ scripts/app.js
@@ -9,6 +9,11 @@ $body.on('touchmove mousemove', (e) => {
   y = e.pageY || e.originalEvent.targetTouches[0].pageY;
 });
 
+$body.on('touchstart', (e) => {
+  x = e.originalEvent.targetTouches[0].pageX;
+  y = e.originalEvent.targetTouches[0].pageY;
+});
+
 var spotlight = function() {
   if (x && y) {
     radial = 'radial-gradient(circle at ' + x + 'px ' + y + 'px, rgba(255, 255, 255, .6) 30%, rgba(0, 0, 0, .6) 100%)';
