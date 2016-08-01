10fb2223e3d937b4262366fa801f5474caf37d2d
Geoffrey Vedernikoff <yefim323@gmail.com>
2016-07-31 18:20:20 -0700

    Push to plugins instead of overwriting

--- webpack-prod.config.js
+++ webpack-prod.config.js
@@ -1,13 +1,13 @@
 var webpack = require('webpack');
 var config = require('./webpack.config');
 
-config.plugins = [
+config.plugins.push(
   new webpack.DefinePlugin({
     'process.env': {
       'NODE_ENV': JSON.stringify('production')
     }
   })
-];
+);
 
 delete config.devtool;
 
