51bf8d1da111470338f3f19532d4081177c14899
Yefim Vedernikoff <yefim323@gmail.com>
2018-08-02 21:08:48 -0700

    Optimize CS

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,16 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
+  optimization: {
+    minimizer: [
+      new OptimizeCSSAssetsPlugin()
+    ]
+  },
   plugins: [
     new MiniCssExtractPlugin({
       filename: '[name].[hash].css',
