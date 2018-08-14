caea7462ebbf8e790081425b3c3cbe54587eaa2f
Yefim Vedernikoff <yefim323@gmail.com>
2018-08-13 20:47:39 -0700

    Minify everything

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
   plugins: [
@@ -33,6 +35,16 @@ module.exports = {
           }
         }
       }
+    ]
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true
+      }),
+      new OptimizeCSSAssetsPlugin()
     ]
   }
 };
