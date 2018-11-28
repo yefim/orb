af4db3cbb2c3977760b2169f7f979a084586e74a
Yefim Vedernikoff <yefim323@gmail.com>
2018-11-27 23:17:00 -0500

    Remove some cruft

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
@@ -39,11 +38,6 @@ module.exports = {
   },
   optimization: {
     minimizer: [
-      new UglifyJsPlugin({
-        cache: true,
-        parallel: true,
-        sourceMap: true
-      }),
       new OptimizeCSSAssetsPlugin()
     ]
   }
